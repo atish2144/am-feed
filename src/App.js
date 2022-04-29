fc7d586d651c06533129142313742678e74fb1cc
amakashk <akash.kshirsagar@angularmaind.in>
2022-04-29 18:02:16 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 
 import './App.css';
 import { Routes, Route } from "react-router-dom"
-import Login from './components/Login';
-
-
-
+import Signup from './components/Signup';
+import Login from './components/Login'
+import Feed from './components/Feed';
+// import Header from './components/Header';
+import Edit from './components/Edit'
 
 function App() {
   return (
     <div className="App">
 
+
+      {/* <Header></Header>
+      <Feed /> */}
       <Routes>
         <Route path="/" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/Feed" element={<Feed />}></Route>
+        <Route path="/Edit" element={<Edit />}></Route>
+
       </Routes>
 
     </div>
